60cf443de7f879246653807363fa8ec7cb4ccf10
ShushilBhusal <sbhusal281@gmail.com>
2025-08-27 11:20:04 +0545

    Add project files and initial structure

---
+++ backend/src/db/connection.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+const connectDB = async ()=>{
+    try{
+        if (!process.env.MONGO_URL) {
+            throw new Error("MONGO_URL environment variable is not defined");
+        }
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Connected to MongoDB");
+    }
+    catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+}
+
+export default connectDB;
